55fc80240ce33fcbbebce77c0bc49b8584ca02cf
dhsjisjs <136228776+dhsjisjs@users.noreply.github.com>
2024-04-18 15:21:27 +0800

    Update FIFO.go

--- FIFO.go
+++ FIFO.go
@@ -1,42 +1,90 @@
-package main
+package FIFO
 
 import (
-	"fmt"
 	"sync"
-	"sync/atomic"
 )
 
+// Node for FIFO
 type FIFONode struct {
 	next     *FIFONode
 	previous *FIFONode
 	data     any
 }
 
+// Use NewFIFO, or you have to init the sync.Pool maunally
+type FIFO struct {
+	//Root node of the duoble linked list
+	root *FIFONode
+	//length of the duoble linked list
+	length int64
+	// Mutex of Pop and Push operations
+	lock sync.Mutex
+	//Sync.pool for FIFONode
+	pool *sync.Pool
+	//noCopy
+	noCopy
+}
+
+// noCopy
+type noCopy struct{}
+
+func (*noCopy) Lock()   {}
+func (*noCopy) Unlock() {}
+
+// Do not use, it is not concurrent safe
 func (f *FIFONode) Next() *FIFONode {
 	return f.next
 }
+
+// Do not use, it is not concurrent safe
 func (f *FIFONode) Previous() *FIFONode {
 	return f.previous
 }
 
+// Do not use, it is not concurrent safe
 func (f *FIFONode) SetNext(next *FIFONode) {
 	f.next = next
 
 }
+
+// Do not use, it is not concurrent safe
 func (f *FIFONode) SetPrevious(pre *FIFONode) {
 	f.previous = pre
 }
 
-type FIFO struct {
-	root   *FIFONode
-	length int64
-	lock   sync.Mutex
+// Use Pop instead
+func (f *FIFONode) GetData() any {
+	return f.data
 }
 
-// Push an element
-func (f *FIFO) Push(node *FIFONode) {
+// Use Push instead
+func (f *FIFONode) SetData(data any) {
+	f.data = data
+}
+
+// Use Pop instead
+func (f *FIFO) pop() *FIFONode {
 	f.lock.Lock()
-	atomic.AddInt64(&f.length, 1)
+	defer f.lock.Unlock()
+	if f.length == 0 {
+		return nil
+	}
+	tmp := f.root.Previous()
+	if f.length == 1 {
+		f.root = nil
+	} else {
+		tmp.Previous().SetNext(f.root)
+		f.root.SetPrevious(tmp.Previous())
+	}
+	f.length--
+	return tmp
+}
+
+// Use Push instead
+func (f *FIFO) push(node *FIFONode) {
+	f.lock.Lock()
+	defer f.lock.Unlock()
+	f.length++
 
 	if f.root != nil {
 		//Push the node into the root
@@ -49,64 +97,49 @@ func (f *FIFO) Push(node *FIFONode) {
 		f.root.SetNext(f.root)
 		f.root.SetPrevious(f.root)
 	}
-	f.lock.Unlock()
-	m.Done()
 }
 
-// Pop an element
-func (f *FIFO) Pop() *FIFONode {
-	f.lock.Lock()
-	if f.length == 0 {
-		return nil
-	}
-	tmp := f.root.Previous()
-	if f.length == 1 {
-		f.root = nil
-	} else {
-		tmp.Previous().SetNext(f.root)
-		f.root.SetPrevious(tmp.Previous())
-	}
-	atomic.AddInt64(&f.length, -1)
-	f.lock.Unlock()
-	return tmp
+// Push an element
+func (f *FIFO) Push(data any) {
+	var p = f.pool.Get().(*FIFONode)
+	p.SetData(data)
+	f.push(p)
 }
 
-// Return the last push element
-func (f *FIFO) Root() *FIFONode {
+// Pop an element if there is one, else it will return nil
+func (f *FIFO) Pop() any {
+	var p = f.pop()
+	if p != nil {
+		tmp := p.GetData()
+		p.SetData(nil)
+		f.pool.Put(p)
+		return tmp
+	}
+	return nil
+}
+
+// Return the head element
+func (f *FIFO) Head() *FIFONode {
 	return f.root
 }
 
-// Return the element will be poped
+// Return the tail element
 func (f *FIFO) Previous() *FIFONode {
 	return f.root.Previous()
 }
 
-var m sync.WaitGroup
+// Return the length of fifo
+func (f *FIFO) GetLength() int {
+	return int(f.length)
+}
 
-func main() {
-	m.Add(10)
-	f := FIFO{}
-	for i := 0; i < 10; i++ {
-		j := i
-		go func() {
-			tmp := FIFONode{
-				data: j,
-			}
-			f.Push(&tmp)
-
-		}()
+// Create an fifo
+func NewFIFO() *FIFO {
+	var f FIFO
+	f.pool = &sync.Pool{
+		New: func() any {
+			return new(FIFONode)
+		},
 	}
-
-	m.Wait()
-	fmt.Println(f.length)
-	m.Add(10)
-	for i := 0; i < 10; i++ {
-		go func() {
-			fmt.Println(f.Pop())
-			m.Done()
-		}()
-	}
-	m.Wait()
-	fmt.Println("all done")
-
+	return &f
 }
